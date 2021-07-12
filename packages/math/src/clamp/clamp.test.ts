bf1f1502c146a09907d124403b0cec2d640b590d
duduluu <duduluu@outlook.com>
2021-07-12 23:05:49 +0800

    test(math): more test for normal clamp

--- packages/math/src/clamp/clamp.test.ts
+++ packages/math/src/clamp/clamp.test.ts
@@ -1,34 +1,84 @@
-import { clamp, clamp01, createClamp } from './clamp';
+import { ceil, floor } from '../standard';
+import {
+  clamp,
+  clamp01,
+  clampCeil,
+  clampFloor,
+  clampRound,
+  clampTrunc,
+  createClamp,
+  createClampCeil,
+  createClampFloor,
+  createClampRound,
+  createClampTrunc,
+} from './clamp';
 
 describe('clamp', () => {
-  test('clamp(), createClamp()', () => {
-    const min = 5.123;
+  test('test normal clamp and integer clamp with round, floor, ceil, trunc', () => {
+    const min = -5.123;
+    const intMin = ceil(min);
     const max = 874.13;
+    const intMax = floor(max);
     const clamper = createClamp(min, max);
+    const clamperRound = createClampRound(min, max);
+    const clamperFloor = createClampFloor(min, max);
+    const clamperCeil = createClampCeil(min, max);
+    const clamperTrunc = createClampTrunc(min, max);
 
-    const data: [number, number][] = [
-      [Number.NEGATIVE_INFINITY, min],
-      [1, min],
-      [3.23, min],
-      [min, min],
+    // value, normal, round, floor, ceil, trunc
+    const data: number[][] = [
+      [Number.NEGATIVE_INFINITY, min, intMin, intMin, intMin, intMin],
+      [-6, min, intMin, intMin, intMin, intMin],
+      [-5.2, min, intMin, intMin, intMin, intMin],
+      [min, min, intMin, intMin, intMin, intMin],
 
-      [654, 654],
-      [10.65, 10.65],
-      [Number.NaN, Number.NaN],
+      [654, 654, 654, 654, 654, 654],
 
-      [max, max],
-      [893.54, max],
-      [900, max],
-      [Number.POSITIVE_INFINITY, max],
+      [10.23, 10.23, 10, 10, 11, 10],
+      [10.5, 10.5, 11, 10, 11, 10],
+      [10.87, 10.87, 11, 10, 11, 10],
+
+      [-1.2, -1.2, -1, -2, -1, -1],
+      [-1.5, -1.5, -1, -2, -1, -1],
+      [-1.8, -1.8, -2, -2, -1, -1],
+
+      [-5.1, -5.1, intMin, intMin, intMin, intMin],
+      [874.1, 874.1, intMax, intMax, intMax, intMax],
+
+      [max, max, intMax, intMax, intMax, intMax],
+      [893.89, max, intMax, intMax, intMax, intMax],
+      [900, max, intMax, intMax, intMax, intMax],
+      [Number.POSITIVE_INFINITY, max, intMax, intMax, intMax, intMax],
+
+      [Number.NaN, Number.NaN, Number.NaN, Number.NaN, Number.NaN, Number.NaN],
     ];
 
-    data.forEach(([value, result]) => {
-      expect(clamp(value, min, max)).toBe(result);
-      expect(clamper(value)).toBe(result);
-    });
-  });
+    data.forEach(
+      ([
+        value,
+        resultNormal,
+        resultRound,
+        resultFloor,
+        resultCeil,
+        resultTrunc,
+      ]) => {
+        expect(clamp(value, min, max)).toBe(resultNormal);
+        expect(clamper(value)).toBe(resultNormal);
 
-  test('clampRound(), createClampRound()', () => {});
+        expect(clampRound(value, min, max)).toBe(resultRound);
+        expect(clamperRound(value)).toBe(resultRound);
+
+        expect(clampFloor(value, min, max)).toBe(resultFloor);
+        expect(clamperFloor(value)).toBe(resultFloor);
+
+        expect(clampCeil(value, min, max)).toBe(resultCeil);
+        expect(clamperCeil(value)).toBe(resultCeil);
+
+        expect(clampTrunc(value, min, max)).toBe(resultTrunc);
+        expect(clamperTrunc(value)).toBe(resultTrunc);
+      },
+    );
+  });
 
   test('clamp01()', () => {
     expect(clamp01(-654.2)).toBe(0);
