1d070ed15eddc4402040733028299dc3146ea7b2
duduluu <duduluu@outlook.com>
2022-06-22 14:23:45 +0800

    chore(color): remove all old files

--- packages/color/src/sRGB/index.ts
+++
@@ -1,9 +0,0 @@
-export * from './float';
-export * from './linear';
-export * from './uint8';
-export * from './keywords';
-
-export * from './gamma';
-export * from './relative-luminance';
-export * from './contrast-ratio';
-export * from './luma';
