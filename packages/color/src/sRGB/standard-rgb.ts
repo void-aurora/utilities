52c0eb34b4c54b02c2e9dd707aaa5859a0f4c7bb
duduluu <duduluu@outlook.com>
2022-06-27 12:18:27 +0800

    doc(color): update tsdoc comment for StandardRGB

--- packages/color/src/sRGB/standard-rgb.ts
+++ packages/color/src/sRGB/standard-rgb.ts
@@ -3,7 +3,14 @@ import { isObject } from '@void-aurora/utils';
 import { clampAlpha, ColorBase } from '../base';
 
 /**
- * The standard-RGB color model.
+ * CSS colors in the sRGB color space are represented by a triplet of values
+ * — red, green, and blue — identifying a point in the sRGB color space [SRGB].
+ * This is an internationally-recognized, device-independent color space,
+ * and so is useful for specifying colors that will be displayed on a computer screen,
+ * but is also useful for specifying colors on other types of devices, like printers.
+ * @see {@link [sRGB Colors](https://www.w3.org/TR/css-color-4/#numeric-srgb)}
+ * @see {@link [Predefined sRGB space](https://www.w3.org/TR/css-color-4/#predefined-sRGB)}
+ * @see {@link [IEC 61966-2-1:1999](https://webstore.iec.ch/publication/6169)}
  */
 export interface StandardRgb extends ColorBase {
   /**
