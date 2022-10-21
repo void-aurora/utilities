67ea6f9d32429464326c8df6cdc9ac47f1eff57d
zhangxiaodong.ddl <zhangxiaodong.ddl@bytedance.com>
2022-10-21 11:23:20 +0800

    feat: move number standard from the package math to the package utils

---
+++ .changeset/funny-days-suffer.md
@@ -0,0 +1,6 @@
+---
+'@void-aurora/math': major
+'@void-aurora/utils': major
+---
+
+Move number standard from the package math to the package utils.
---
+++ packages/utils/src/number/standard.ts
@@ -0,0 +1,20 @@
+export const {
+  Number: {
+    // es5
+    MAX_VALUE,
+    MIN_VALUE,
+    NaN,
+    NEGATIVE_INFINITY,
+    POSITIVE_INFINITY,
+    // es2015
+    EPSILON,
+    isFinite,
+    isInteger,
+    isNaN,
+    isSafeInteger,
+    MAX_SAFE_INTEGER,
+    MIN_SAFE_INTEGER,
+    parseFloat,
+    parseInt,
+  },
+} = globalThis;
